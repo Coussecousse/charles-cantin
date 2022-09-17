0d14527595812edd3ed76924e3a11316627d7558
Erika <adob.e@hotmail.com>
2022-09-17 09:31:36 +0200

    fix: link home stay active

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,13 +1,16 @@
 import React from "react";
-import paths from "../../../config/paths";
+import { NavLink } from "react-router-dom";
 import classes from './Navigation.module.css';
 
+import paths from "../../../config/paths";
 import NavigationItem from './NavigationItem/NavigationItem'
 
 export default function Navigation(){
     return (
         <ul className={classes.UlHeader}>
-            <NavigationItem exact to={paths.HOME}>Accueil</NavigationItem>
+             <li>
+                <NavLink to={paths.HOME} end className={({isActive}) => (isActive ? 'link active' : 'link')}>Accueil</NavLink>
+            </li>
             <NavigationItem to={paths.GALLERY}>Galerie</NavigationItem>
             <NavigationItem to={paths.SERVICES}>Prestations</NavigationItem>
             <NavigationItem to={paths.CONTACT}>Contact</NavigationItem>
