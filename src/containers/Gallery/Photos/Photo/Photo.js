6d35ac41326bc4da9ca721a3bb99691d6eb6dc2f
Erika <adob.e@hotmail.com>
2022-10-06 17:00:00 +0200

    fix: photos and cards size test for netlify

--- src/containers/Gallery/Photos/Photo/Photo.js
+++ src/containers/Gallery/Photos/Photo/Photo.js
@@ -3,23 +3,23 @@ import React, { useEffect } from "react";
 export default function Photo(props) {
     function getSize() {
         switch(props.size){
-            case '3/3\r' : 
+            case '3/3' : 
                 return {
                     gridColumn: 'span 3'
                 }
-            case '2/3\r' : 
+            case '2/3' : 
                 return {
                     gridColumn: 'span 2'
                 }
-            case '1/3\r': 
+            case '1/3': 
                 return {
                     gridColumn: 'span 1'
             }
-            case '2/2\r': 
+            case '2/2': 
                 return {
                     gridColumn: 'span 2'
                 }
-            case '1/2\r': 
+            case '1/2': 
                 return {
                     gridColumn: 'span 1'
                 }
@@ -30,8 +30,6 @@ export default function Photo(props) {
         }
     }
 
-    console.log('hello')
-    console.log(props.size)
     return(
         <div style={getSize()}>
             <img className="photoAnimation" src={props.src} alt={props.alt} style={{objectPosition : props.posImg}}></img>
