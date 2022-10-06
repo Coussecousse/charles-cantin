65f2785c415d3fabe1b697652f3f41e1bbb41840
Erika <adob.e@hotmail.com>
2022-10-06 16:50:31 +0200

    fix : photos size

--- src/containers/Gallery/Photos/Photo/Photo.js
+++ src/containers/Gallery/Photos/Photo/Photo.js
@@ -3,23 +3,23 @@ import React, { useEffect } from "react";
 export default function Photo(props) {
     function getSize() {
         switch(props.size){
-            case '3/3' : 
+            case '3/3\r' : 
                 return {
                     gridColumn: 'span 3'
                 }
-            case '2/3' : 
+            case '2/3\r' : 
                 return {
                     gridColumn: 'span 2'
                 }
-            case '1/3': 
+            case '1/3\r': 
                 return {
                     gridColumn: 'span 1'
             }
-            case '2/2': 
+            case '2/2\r': 
                 return {
                     gridColumn: 'span 2'
                 }
-            case '1/2': 
+            case '1/2\r': 
                 return {
                     gridColumn: 'span 1'
                 }
