75623c72fae33409e59f31aaaf4fdb73806352dd
Erika <adob.e@hotmail.com>
2022-10-06 16:52:11 +0200

    fix: cards size

--- src/containers/Services/Cards/Card/Card.js
+++ src/containers/Services/Cards/Card/Card.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Card(props) {
     function getSize() {
         switch(props.size){
-            case '2/2\r': 
+            case '2/2': 
                 return 'span 2';
-            case '1/2\r':
+            case '1/2':
                 return 'span 1';
             default: 
                 return 'span 2';
