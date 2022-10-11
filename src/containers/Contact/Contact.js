43edf4468745a052782e678092cb0d6dead492b9
Erika <adob.e@hotmail.com>
2022-10-11 10:06:16 +0200

    fix: redirect forms when send

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -25,7 +25,7 @@ export default function Contact(props) {
                 <div className={[classes.Form, "contactAnimation"].join(' ')}>
                     <form name="contact" 
                           method="POST" 
-                          action={() => console.log('coucou')}
+                          action='/#/contact/success'
                           >
                         <input type="hidden" name="form-name" value="contact"/>
                         <div hidden>
