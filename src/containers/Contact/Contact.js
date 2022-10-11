355668bc672a12e26af0adc2b0615dbe09d5509d
Erika <adob.e@hotmail.com>
2022-10-11 10:14:18 +0200

    fix: useNavigate is not defined

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useNavigate } from "react";
 import classes from './Contact.module.css';
 
 import servicesData from '../../services.json';
